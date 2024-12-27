80829ebc7219303d1ea7ac9be8b2e597c8dc7797
Parhlad Singh <142223673+parhladSingh@users.noreply.github.com>
2024-12-27 15:48:26 +0530

    Update dbConnect.ts

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,30 +1,3 @@
-// import { connect } from "http2";
-// import mongoose from "mongoose";
-
-// type ConnectionObject =  {
-//     isConnected?:number
-// }
-
-// const connection : ConnectionObject = {}
-
-// async function dbConnect(): Promise<void> {
-//     if(connection){
-//         console.log("Already connected to the database");
-//         return
-//     }
-//     try{
-//        const db =  await mongoose.connect(process.env.MONGODB_URI||'',{})
-//         connection.isConnected = db.connections[0].readyState
-//         console.log("DB Connected successfully");
-//     }catch(error){
-
-//         console.log("DB is not connected",error)
-//         process.exit()
-//     }
-// }
-
-// export default dbConnect;
-
 import mongoose from "mongoose";
 
 export async function connect(){
